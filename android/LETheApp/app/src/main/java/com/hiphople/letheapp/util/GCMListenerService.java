a418adc7d4749447eb3d173acd2bcaad45d6e21e
Hyoyeol Maeng <maenghy@gmail.com>
2016-03-02 23:19:55 +0900

    Fixed to show correct title of Push Msg

--- android/LETheApp/app/src/main/java/com/hiphople/letheapp/util/GCMListenerService.java
+++ android/LETheApp/app/src/main/java/com/hiphople/letheapp/util/GCMListenerService.java
@@ -31,7 +31,9 @@ public class GCMListenerService extends GcmListenerService{
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
+        String title = data.getString("title");
         Log.d(TAG, "From: " + from);
+        Log.d(TAG, "Title: " + title);
         Log.d(TAG, "Message: " + message);
 
         if (from.startsWith("/topics/")) {
@@ -51,7 +53,7 @@ public class GCMListenerService extends GcmListenerService{
          * In some cases it may be useful to show a notification indicating to the user
          * that a message was received.
          */
-        sendNotification(message);
+        sendNotification(title, message);
         // [END_EXCLUDE]
     }
     // [END receive_message]
@@ -61,7 +63,7 @@ public class GCMListenerService extends GcmListenerService{
      *
      * @param message GCM message received.
      */
-    private void sendNotification(String message) {
+    private void sendNotification(String title, String message) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
@@ -70,7 +72,7 @@ public class GCMListenerService extends GcmListenerService{
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("GCM Message")
+                .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
