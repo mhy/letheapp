e6719e7f6bf33003f18c859dc0bbcf0d2808e879
Hyoyeol Maeng <maenghy@gmail.com>
2016-03-13 21:27:41 +0900

    Added custom error page

--- android/LETheApp/app/src/main/java/com/hiphople/letheapp/webview/LEWebViewClient.java
+++ android/LETheApp/app/src/main/java/com/hiphople/letheapp/webview/LEWebViewClient.java
@@ -3,6 +3,8 @@ package com.hiphople.letheapp.webview;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -15,6 +17,7 @@ import com.hiphople.letheapp.R;
  */
 public class LEWebViewClient extends WebViewClient{
     private static final String TAG = "LEWebViewClient";
+    private static final String ERROR_PAGE = "file:///android_asset/error.html";
 
     private ProgressBar mProgBar;
 
@@ -38,5 +41,10 @@ public class LEWebViewClient extends WebViewClient{
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
         mProgBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+        view.loadUrl(ERROR_PAGE);
     }
 }
