547d877252970f7822b0d11f4cadd1591b106959
Hyoyeol Maeng <maenghy@gmail.com>
2016-04-02 18:17:16 +0900

    Added alert message for kitkat users : "attaching files" is not available

--- android/LETheApp/app/src/main/java/com/hiphople/letheapp/webview/LEWebViewClient.java
+++ android/LETheApp/app/src/main/java/com/hiphople/letheapp/webview/LEWebViewClient.java
@@ -1,7 +1,11 @@
 package com.hiphople.letheapp.webview;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -18,17 +22,47 @@ import com.hiphople.letheapp.R;
 public class LEWebViewClient extends WebViewClient{
     private static final String TAG = "LEWebViewClient";
     private static final String ERROR_PAGE = "file:///android_asset/error.html";
+    private static final String PATTERN_BOARD_WRITE = ".*dispBoardWrite.*";
+    private static final String[] RELEASE_OF_BUG = {"4.4", "4.4.1", "4.4.2"};
 
+    private Activity mAct;
     private ProgressBar mProgBar;
 
     public LEWebViewClient(Activity activity) {
+        mAct = activity;
         mProgBar = (ProgressBar)activity.findViewById(R.id.progressBar);
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        return false;
-        //return super.shouldOverrideUrlLoading(view, url);
+    public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+        boolean returnValue = false;
+        if(url.matches(PATTERN_BOARD_WRITE)){
+            boolean isVersionOfBug = false;
+
+            for(String version : RELEASE_OF_BUG){
+                if (version.equals(Build.VERSION.RELEASE)){
+                    isVersionOfBug = true;
+                    break;
+                }
+            }
+
+            if(isVersionOfBug){
+                AlertDialog.Builder builder = new AlertDialog.Builder(mAct);
+                builder.setMessage(R.string.specific_kitkat_error_meessage)
+                        .setTitle(R.string.specific_kitkat_error_title)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                view.loadUrl(url);
+                            }
+                        })
+                        .setNegativeButton(R.string.no, null);
+                builder.show();
+
+                returnValue = true;
+            }
+        }
+        return returnValue;
     }
 
     @Override
